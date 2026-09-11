edc852bdac2a1cd3d4986d968d27b4269aed09eb
Vencislav Atanasov <git@6bez10.info>
2026-09-11 14:39:54 +0300

    newlines

--- RealSIDpy/RealSID.py
+++ RealSIDpy/RealSID.py
@@ -3,19 +3,24 @@
 '''
 RealSID.py
 Version 1.0
+
 Copyright (c) 2013, A.T.Brask (atbrask[at]gmail[dot]com)
 All rights reserved,
+
 Very rudimentary SID player for demonstrating the RealSIDShield Arduino shield.
 Basically, it emulates a 6502 CPU with 64KB of memory. No VIC or CIA chips.
 It works by running the play routine at ~50 Hz. After each run, it updates the
 registers in real SID chip connected via the Arduino.
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
+
 1. Redistributions of source code must retain the above copyright notice, this
    list of conditions and the following disclaimer.
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
